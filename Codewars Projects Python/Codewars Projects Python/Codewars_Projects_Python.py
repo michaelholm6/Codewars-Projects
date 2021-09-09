e35aa7d34fcc2e93c1a4a4daa0541431f98a1d5e
michaelholm6 <mdholm@iastate.edu>
2021-09-09 09:55:30 -0500

    String Splitter Function

--- Codewars Projects Python/Codewars Projects Python/Codewars_Projects_Python.py
+++ Codewars Projects Python/Codewars Projects Python/Codewars_Projects_Python.py
@@ -1,1 +1,13 @@
+def solution(s):
+    Answer = []
+    temporary_string = ""
+    for letter in s:
+        temporary_string += letter
+        if len(temporary_string) > 1:
+            Answer.append(temporary_string)
+            temporary_string = ""
+    if len(s) % 2 == 1:
+        Answer.append(s[-1]+"_")
 
+if __name__ == "__main__":
+    solution("abcdefg")
